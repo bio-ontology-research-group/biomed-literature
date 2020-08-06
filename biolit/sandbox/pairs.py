47bdc4526e46fe55895b88606c18e22a8a63072a
Mahmut Uludag <mahmut.uludag@kaust.edu.sa>
2020-08-06 13:01:09 +0300

    initial work for including child concepts in concept queries

--- biolit/sandbox/pairs.py
+++ biolit/sandbox/pairs.py
@@ -1,17 +1,17 @@
 from biolit.esquery import termcounts
 from biolit.npmi import scores
-from biolit.sandbox.terms import read_terms
+from biolit.sandbox.terms import read_terms, term_esquery
 import json
 
 def pairs(terms1, rnode1, terms2, rnode2, index='pmc'):
     r = list()
-    for i in terms1[rnode1]['children']:
+    for i in terms1[rnode1]['children'].union({rnode1}):
         cname = terms1[i]['name']
-        cq = cname.replace(' ', ' AND ')
+        cq = term_esquery(terms1, i)
         xpm = termcounts(cq, index=index)
-        for j in terms2[rnode2]['children']:
+        for j in terms2[rnode2]['children'].union({rnode2}):
             dname = terms2[j]['name']
-            dq = dname.replace(' ', ' AND ')
+            dq = term_esquery(terms2, j)
             s = scores(cq, dq, index=index, x=xpm)
             if s is not None:
                 r.append({
@@ -21,14 +21,18 @@ def pairs(terms1, rnode1, terms2, rnode2, index='pmc'):
                 })
     return r
 
-
 def test_do_pairs():
     do = read_terms("../../../ontrepository/doid.obo")
-    assert len(do['DOID:874']['children']) == 6
-    assert len(do['DOID:8469']['children']) == 2
+    assert len(do['DOID:874']['children']) == 6   # bacterial pneumonia
+    assert len(do['DOID:8469']['children']) == 2  # influenza
+    assert len(do['DOID:0060519']['children']) == 5  # beta-lactam allergy
     r = pairs(do, 'DOID:874', do, 'DOID:8469', index='pubmed')
     print(json.dumps(r, indent=4))
-    assert len(r) == 8
+    assert len(r) == 11
     r = pairs(do, 'DOID:874', do, 'DOID:8469')
     print(json.dumps(r, indent=4))
-    assert len(r) == 8
+    assert len(r) == 11
+    r = pairs(do, 'DOID:874', do, 'DOID:0060519', index='pubmed')
+    assert len(r) == 4
+    r = pairs(do, 'DOID:874', do, 'DOID:0060519')
+    assert len(r) == 4
